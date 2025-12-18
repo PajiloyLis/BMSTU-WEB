5e22dc3bc8418e54dfaf9a71a6a88dd0fd4fa4a9
Bugakov Ivan <bugakov0601@yandex.ru>
2025-12-18 11:57:05 +0300

    Updated Nginx configuration

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// PrimeNG
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    MenubarModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/Project.Core/Models/IHierarchy.cs
@@ -0,0 +1,6 @@
+namespace Project.Core.Models;
+
+public interface IHierarchy
+{
+    
+}
