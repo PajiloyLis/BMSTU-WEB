ec17a213a36303a4687f07d445227f5691c0670b
Ivan <bugakov0601@yandex.ru>
2026-03-20 00:18:26 +0300

    remove shit from lab 7

--- src/Project.HttpServer/ExternalServices/AgePrediction/IAgePredictionClient.cs
+++
@@ -1,8 +0,0 @@
-using Project.Dto.Http.External;
-
-namespace Project.HttpServer.ExternalServices.AgePrediction;
-
-public interface IAgePredictionClient
-{
-    Task<AgePredictionDto> PredictAgeAsync(string name, CancellationToken cancellationToken = default);
-}
